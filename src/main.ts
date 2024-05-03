4d8de4d54b0e4a065a0b15784a336429d36e0aae
nicolo0312 <nicomoran_2@hotmail.com>
2024-05-03 13:39:13 -0300

    add global api prefix

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = 3000 
-
+  const PORT = process.env.PORT; 
+  
+  app.setGlobalPrefix('api')
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist:true,
